5cdf48e60b15c9707339bb1de0f1b4f2b2798f84
ismatech <i.kyrbanov@pr-sz.ru>
2017-08-29 02:07:48 +0700

    fixed class Vector

--- game.js
+++ game.js
@@ -11,17 +11,13 @@ class Vector {
     this.y = y;
   }
   plus(vector) {
-    try {
       if (vector instanceof Vector) {
         const newObject = new Vector();
         newObject.x = this.x + vector.x;
         newObject.y = this.y + vector.y; 
         return newObject;
-      } else throw new Error('Можно прибавлять к вектору только вектор типа Vector');
-    }
-    catch (error) {
-      throw error;
-    }
+      } 
+       throw new Error('Можно прибавлять к вектору только вектор типа Vector');
   }
   times (multiplier) {
     const newVector = new Vector();
@@ -36,45 +32,6 @@ const finish = start.plus(moveTo.times(2));
 console.log(`Исходное расположение: ${start.x}:${start.y}`);
 console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
 
-// class Actor {
-//   constructor (coords={x:0,y:0}, objSize={height:1, width:1}, objSpeed={x:0,y:0}) {
-//     try {  
-//       if (coords !instanceof Vector || objSize !instanceof Vector || objSpeed !instanceof Vector) {
-//         throw new Error('Аргументы должны быть типа Vector');
-//       }
-//     } catch (e) {
-//         throw e;
-//     }
-//   }
-//   get pos (vector) {
-//     this.x = coords.x;
-//     this.y = coords.y;
-//   }
-//   get size (vector) {
-//     this.height = vector.height;
-//     this.width = vector.width;
-//   }
-//   get speed (vector) {
-//     this.x += vector.x;
-//     this.y += vector.y;
-//   }
-//   act () {}
-//   set left () {
-
-//   }
-//   set right () {
-
-//   }
-//   set top () {
-
-//   }
-//   set bottom () {
-
-//   }
-//   set type () {
-//    return 'actor'; 
-//   }
-// }
 /*
 2. После этого вы уже сможете запустить игру.
   ```javascript
