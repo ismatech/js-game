4f6396ff2d4f165ee0b4c77df1127cc8c25c614c
ismatech <i.kyrbanov@pr-sz.ru>
2017-09-03 23:50:09 +0700

    solved conflicts at new PC and wrote class Level

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 /*
 ## Процесс и порядок реализации
@@ -18,7 +17,7 @@ class Vector {
       newObject.y = this.y + vector.y; 
       return newObject;
     } 
-      throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+      throw new Error('Можно прибавлять к вектору только вектор типа Vector!');
   }
   times (multiplier) {
     const newVector = new Vector();
@@ -28,18 +27,18 @@ class Vector {
   }
 }
 /*checking code in console for the 1st class*/
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
+// const start = new Vector(30, 50);
+// const moveTo = new Vector(5, 10);
+// const finish = start.plus(moveTo.times(2));
+// console.log(`Исходное расположение: ${start.x}:${start.y}`);
+// console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
 
 class Actor {
   constructor(pos = new Vector(), size = new Vector(1, 1), speed = new Vector()) {
     if (!(pos instanceof Vector) ||
         !(size instanceof Vector) ||
         !(speed instanceof Vector)) {
-          throw Error('arguments error');
+          throw Error('Объекты должны быть типа Vector!');
         }
     this.pos = pos;
     this.size = size;
@@ -71,7 +70,7 @@ class Actor {
 
   isIntersect(actor) {
     if (!(actor instanceof Actor) || actor === undefined) {
-          throw Error('arguments error');
+          throw Error('Объект должен быть типа Actor!');
     }
 
     if (actor === this || actor.size.x < 0 || actor.size.y < 0) {
@@ -82,166 +81,167 @@ class Actor {
   }
 }
 /*checking code in console for the 2nd class*/
-const items = new Map();
-const player = new Actor();
-items.set('Игрок', player);
-items.set('Первая монета', new Actor(new Vector(10, 10)));
-items.set('Вторая монета', new Actor(new Vector(15, 5)));
+// const items = new Map();
+// const player = new Actor();
+// items.set('Игрок', player);
+// items.set('Первая монета', new Actor(new Vector(10, 10)));
+// items.set('Вторая монета', new Actor(new Vector(15, 5)));
 
-function position(item) {
-  return ['left', 'top', 'right', 'bottom']
-    .map(side => `${side}: ${item[side]}`)
-    .join(', ');  
-}
+// function position(item) {
+//   return ['left', 'top', 'right', 'bottom']
+//     .map(side => `${side}: ${item[side]}`)
+//     .join(', ');  
+// }
 
-function movePlayer(x, y) {
-  player.pos = player.pos.plus(new Vector(x, y));
-}
+// function movePlayer(x, y) {
+//   player.pos = player.pos.plus(new Vector(x, y));
+// }
 
-function status(item, title) {
-  console.log(`${title}: ${position(item)}`);
-  if (player.isIntersect(item)) {
-    console.log(`Игрок подобрал ${title}`);
-  }
-}
+// function status(item, title) {
+//   console.log(`${title}: ${position(item)}`);
+//   if (player.isIntersect(item)) {
+//     console.log(`Игрок подобрал ${title}`);
+//   }
+// }
 
-items.forEach(status);
-movePlayer(10, 10);
-items.forEach(status);
-movePlayer(5, -5);
-items.forEach(status);
-/*
-2. После этого вы уже сможете запустить игру.
-  ```javascript
-  const grid = [
-    new Array(3),
-    ['wall', 'wall', 'lava']
-  ];
-  const level = new Level(grid);
-  runLevel(level, DOMDisplay);
-  ```
+// items.forEach(status);
+// movePlayer(10, 10);
+// items.forEach(status);
+// movePlayer(5, -5);
+// items.forEach(status);
 
-  На экране отобразится схема уровня. Узнайте подробнее про функцию `runLevel` и класс `DOMDisplay` ниже.
-
-3. Реализуйте `LevelParser`, что позволит вам описывать уровни с помощью текстовой схемы:
-  ```javascript
-  const schema = [
-    '         ',
-    '         ',
-    '         ',
-    '         ',
-    '     !xxx',
-    '         ',
-    'xxx!     ',
-    '         '
-  ];
-  const parser = new LevelParser();
-  const level = parser.parse(schema);
-  runLevel(level, DOMDisplay);
-  ```
-4. Реализуйте `Player`, поместите его символ на схему и добавьте словарь при создании парсера:
-  ```javascript
-  const schema = [
-    '         ',
-    '         ',
-    '         ',
-    '         ',
-    '     !xxx',
-    ' @       ',
-    'xxx!     ',
-    '         '
-  ];
-  const actorDict = {
-    '@': Player
-  }
-  const parser = new LevelParser(actorDict);
-  const level = parser.parse(schema);
-  runLevel(level, DOMDisplay);
-  ```
-5. Реализуйте другие движущиеся объекты игрового поля и помещайте их символы на схему и в словарь парсера.
-6. Реализуйте загрузку уровней с помощью функции `loadLevels` и запуск игры с помощью `runGame`.
-7. Когда игрок пройдет все уровни, используйте функцию `alert`, чтобы сообщить о победе.
-=======
-'use strict';
-/*
-## Процесс и порядок реализации
-
-Для того, чтобы максимально просто и быстро получить базовый рабочий вариант проекта, рекомендуем придерживаться следующего плана разработки:
-
-1. Реализовать базовые классы игры: `Vector`, `Actor` и `Level`.*/
-class Vector {
-	constructor(x=0, y=0) {
-    this.x = x;
-    this.y = y;
-  }
-  plus(vector) {
-      if (vector instanceof Vector) {
-        const newObject = new Vector();
-        newObject.x = this.x + vector.x;
-        newObject.y = this.y + vector.y; 
-        return newObject;
-      } 
-       throw new Error('Можно прибавлять к вектору только вектор типа Vector');
-  }
-  times (multiplier) {
-    const newVector = new Vector();
-    newVector.x = this.x * multiplier;
-    newVector.y = this.y * multiplier;
-    return newVector;
-  }
-}
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
-class Actor {
-  constructor(pos = new Vector(), size = new Vector(1, 1), speed = new Vector()) {
-    if (!(pos instanceof Vector) ||
-        !(size instanceof Vector) ||
-        !(speed instanceof Vector)) {
-          throw Error('arguments error');
-        }
-    this.pos = pos;
-    this.size = size;
-    this.speed = speed;
-    this._type = 'actor';
+class Level {
+  constructor(grid = [], actors = []) {
+    this.grid = grid;
+    this.actors = actors;
+    this.height = grid.length;
+    this.width = grid.reduce((ceil, row) => row.length > ceil ? row.length : ceil, 0);
+    this.status = null;
+    this.finishDelay = 1;
+    this.player = actors.find(elem => elem.type === 'player');
   }
 
-  act() {}
-
-  get left() {
-    return this.pos.x;
+  isFinished() {
+    return this.status !== null && this.finishDelay < 0 ? true : false;
   }
 
-  get top() {
-    return this.pos.y;
-  }
-
-  get right() {
-    return this.pos.x + this.size.x;
-  }
-
-  get bottom() {
-    return this.pos.y + this.size.y;
-  }
-
-  get type() {
-    return this._type;
-  }
-
-  isIntersect(actor) {
-    if (!(actor instanceof Actor) || actor === undefined) {
-          throw Error('arguments error');
+  actorAt(actor) {
+    if (!actor || !(actor instanceof Actor)) {
+      throw Error('Объект должен быть типа Actor!');
     }
+    return this.actors.find(el => el.isIntersect(actor));
+  }
 
-    if (actor === this || actor.size.x < 0 || actor.size.y < 0) {
-          return false;
+  isObstacle(x, y) {
+    const wall = 'wall';
+    const lava = 'lava';
+    const grid = this.grid;
+    return (grid[y] && grid[y][x] && ((grid[y][x] === wall) || (grid[y][x] === lava)));
+  }
+
+  obstacleAt(nextPos, size) {
+    if (!(nextPos instanceof Vector) ||
+        !(size instanceof Vector)) {
+      throw Error('Объекты должны быть типа Vector!');
+    }
+    const sizeX = size.x - 0.0001;
+    const sizeY = size.y - 0.0001;
+    const grid = this.grid;
+    const x = nextPos.x;
+    const y = nextPos.y;
+    const left = Math.floor(x);
+    const top = Math.floor(y);
+    const bottom = Math.floor(y + sizeY);
+    const right = Math.floor(x + sizeX);
+    const middle = Math.round(top + sizeY / 2);
+
+    if (this.isObstacle(left, top)) {
+      return grid[top][left];
+    }
+    if (this.isObstacle(right, top)) {
+      return grid[top][right];
+    }
+    if (this.isObstacle(left, bottom)) {
+        return grid[bottom][left];
+    }
+    if (this.isObstacle(right, bottom)) {
+        return grid[bottom][right];
+    }
+    if (this.isObstacle(left, middle)) {
+        return grid[middle][left];
+    }
+    if (this.isObstacle(right, middle)) {
+        return grid[middle][right];
+    }
+    if (left < 0 || x + sizeX > this.width || top < 0) {
+      return 'wall';
+    }
+    if (y + sizeY > this.height) {
+      return 'lava';
+    }
+  }
+
+  removeActor(actor) {
+    this.actors = this.actors.filter(elem => elem !== actor);
+  }
+
+  noMoreActors(type) {
+    const result = this.actors.filter(elem => elem.type === type);
+    return result.length > 0 ? false : true;
+  }
+
+  playerTouched(type, actor) {
+    if (this.status !== null) {
+      return;
+    }
+    if (type === 'lava' || type === 'fireball') {
+      this.status = 'lost';
+    } else if (type === 'coin') {
+      this.removeActor(actor);
+      if (!this.actors.find(elem => elem.type === 'coin')) {
+        this.status = 'won';
       }
-
-  return !(actor.left >= this.right || actor.right <= this.left || actor.top >= this.bottom || actor.bottom <= this.top);
+    }
   }
 }
+/*checking code in console for class Level*/
+const grid = [
+  [undefined, undefined],
+  ['wall', 'wall']
+];
+
+function MyCoin(title) {
+  this.type = 'coin';
+  this.title = title;
+}
+MyCoin.prototype = Object.create(Actor);
+MyCoin.constructor = MyCoin;
+
+const goldCoin = new MyCoin('Золото');
+const bronzeCoin = new MyCoin('Бронза');
+const player = new Actor();
+const fireball = new Actor();
+
+const level = new Level(undefined, [ goldCoin, bronzeCoin, player, fireball ]);
+
+level.playerTouched('coin', goldCoin);
+level.playerTouched('coin', bronzeCoin);
+
+if (level.noMoreActors('coin')) {
+  console.log('Все монеты собраны');
+  console.log(`Статус игры: ${level.status}`);
+}
+
+const obstacle = level.obstacleAt(new Vector(1, 1), player.size);
+if (obstacle) {
+  console.log(`На пути препятствие: ${obstacle}`);
+}
+
+const otherActor = level.actorAt(player);
+if (otherActor === fireball) {
+  console.log('Пользователь столкнулся с шаровой молнией');
+}
+
 /*
 2. После этого вы уже сможете запустить игру.
   ```javascript
@@ -293,5 +293,4 @@ class Actor {
 5. Реализуйте другие движущиеся объекты игрового поля и помещайте их символы на схему и в словарь парсера.
 6. Реализуйте загрузку уровней с помощью функции `loadLevels` и запуск игры с помощью `runGame`.
 7. Когда игрок пройдет все уровни, используйте функцию `alert`, чтобы сообщить о победе.
->>>>>>> 7740d3dd681edf7c1c6ea5910d7c213e3f51e4a8
 */
